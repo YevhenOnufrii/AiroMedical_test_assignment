b1a3776cbd4648a10ba99f606cc5d4e4c936b172
Yevhen <onufrii.socnet@gmail.com>
2023-07-14 16:40:38 +0300

    add recipe card

--- src/app/page.js
+++ src/app/page.js
@@ -11,11 +11,31 @@ function Header() {
   )
 }
 
+function Recipe() {
+  return (
+    <li className="recipe__card card">
+      <div className="card--img">
+        <img src="https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/AN_images/eggs-breakfast-avocado-1296x728-header.jpg?w=1155&h=1528" />
+      </div>
+      <div className="card--text">
+        <div className="card--title">Recipe Title</div>
+        <div className="card--sub">
+          A short description of the recipe will go here. Lorem Ipsum dolor sit amet amet consecteur
+          A short description of the recipe will go here. Lorem Ipsum dolor sit amet consecteur.
+        </div>
+        <a href="#" className="cta">
+          View Recipe
+        </a>
+      </div>
+    </li>
+  )
+}
+
 function RecipesList() {
   return (
     <div className="recipes">
       <ul className="recipes__list">
-        <li className="recipe card"></li>
+        <Recipe />
       </ul>
     </div>
   )
@@ -27,6 +47,7 @@ export default function App() {
       <Wrapper>
         <Container>
           <Header />
+          <RecipesList />
         </Container>
       </Wrapper>
     </div>
