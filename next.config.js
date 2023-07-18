9419200d1f4340f810ba10d9560e78ac4c0b4b8e
Yevhen <112400935+YevhenOnufrii@users.noreply.github.com>
2023-07-18 10:46:04 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: {
-    // trailingSlash: true,
-    output: 'export',
-    // images: { unoptimized: true },
-    // export: {
-    //   basePath: '/airoMedical_test_assignment',
-    // },
-  },
+  output: 'export',
+  // output: {
+  //   // trailingSlash: true,
+  //   output: 'export',
+  //   // images: { unoptimized: true },
+  //   // export: {
+  //   //   basePath: '/airoMedical_test_assignment',
+  //   // },
+  // },
 }
 
 module.exports = nextConfig
